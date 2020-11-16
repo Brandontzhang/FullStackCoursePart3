367b37aa467b7ad4cc1410c535e8de37f5b64028
brandontzhang <zhangtbrandon@gmail.com>
2020-11-15 21:50:43 -0500

    port

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { response } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 morgan.token('body', function(req, res) {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
@@ -67,7 +69,7 @@ app.post('/api/persons', (req, res) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
